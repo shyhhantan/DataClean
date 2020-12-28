4c92336bc00994b98f063a01a4425846886cd302
shyhhan tan <shyhhan@gmail.com>
2020-12-28 14:16:23 -0500

    Cleaned up the steps and text

--- run_analysis.r
+++ run_analysis.r
@@ -51,124 +51,73 @@ recursive_replace_lowercase()
 list.files("./data/uci_har_dataset/")
 
 ## Read in the activity_labels.txt and features.txt" files.
-# Read the activity_labels.txt file as activity_labels and name the columns:
-activity_labels <- read.table("./data/uci_har_dataset/activity_labels.txt")
-colnames(activity_labels, do.NULL = FALSE)
-colnames(activity_labels) <- c("activity_label","activity")
-head(activity_labels)
 
 # Read the features.txt" file and mae as features and name the columns:
 features<- read.table("./data/uci_har_dataset/features.txt")
-colnames(features, do.NULL = FALSE)
-colnames(features) <- c("feature_no","feature")
-str(features)
+
+# Read the activity_labels.txt file as activity_labels and name the columns:
+activity_labels <- read.table("./data/uci_har_dataset/activity_labels.txt")
+colnames(activity_labels) <- c("activity_label","activity")
 
 ## Read in the training set files and merge the files by using cbind
-# Get a list of files in the train directory:
-list.files("./data/uci_har_dataset/train")
 
-# Read in the subject_train.txt file as train_subjects and label the column name as "subject":
+# Read in the trainig set files
 train_subjects<- read.table("./data/uci_har_dataset/train/subject_train.txt")
-colnames(train_subjects, do.NULL = FALSE)
-colnames(train_subjects) <- c("subject")
-head(train_subjects)
-
-# Read in the y_train.txt file as train_label and label the column name as "activity_label":
 train_label<- read.table("./data/uci_har_dataset/train/y_train.txt")
-colnames(train_label, do.NULL = FALSE)
-colnames(train_label) <- c("activity_label")
-head(train_label)
-
-# Read in the x_train.txt file as train_set, 
-# name the columnns by using the 2nd column in the "features" file:
 train_set <- read.table("./data/uci_har_dataset/train/x_train.txt")
-train_set_colnames <- features[,2]
-colnames(train_set, do.NULL = FALSE)
-colnames(train_set) <- train_set_colnames
-str(train_set)
-head(train_set)[1:5,1:5]
-# merge the train_subjects, train_label and train_set data by using cbind:
+
+# Merge the train_subjects, train_label and train_set data by using cbind:
 traindf <- cbind(train_subjects,train_label,train_set)
 
-#check the features of the merged traindf dataframe
-dim(traindf)
-
 ## Read in the test set files and merge the files by using cbind
-# Get a list of files in the test directory:
-list.files("./data/uci_har_dataset/test")
-  
-
-# Read in the subject_test.txt file as train_subjects and label the column name as "subject":
 test_subjects<- read.table("./data/uci_har_dataset/test/subject_test.txt")
-colnames(test_subjects, do.NULL = FALSE)
-colnames(test_subjects) <- c("subject")
-head(test_subjects)
-
-# Read in the test label file and name the column as "activity_label"
 test_label<- read.table("./data/uci_har_dataset/test/y_test.txt")
-colnames(test_label, do.NULL = FALSE)
-colnames(test_label) <- c("activity_label")
-head(test_label)
-
-# Read in the x_train.txt file as test_set
 test_set <- read.table("./data/uci_har_dataset/test/x_test.txt")
-test_set_colnames <- features[,2]
-colnames(test_set, do.NULL = FALSE)
-colnames(test_set) <- test_set_colnames
 
 # Merge the test_subjects, test_label and test_set data by using cbind:
 testdf <- cbind(test_subjects, test_label, test_set)
 
-#check the features of the merged testdf dataframe
-dim(testdf)
-head(testdf)[1:5,1:5]
-
 ## Merge the test and training sets by using rbind
 mergedf <- rbind(traindf, testdf)
 
-#check the features of the merged dataframe mergedf
-dim(mergedf)
-head(mergedf)[1:5,1:5]
+# Name the columns of the dataframe, using the variables description in the features file:
+train_set_colnames <- features[,2]
+colnames(mergedf) <- c("subject", "activity_label", train_set_colnames)
+str(mergedf)
+head(mergedf)[,1:5]
+
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-
 # Load the dplyr package
 library(dplyr)
 
-# Select the variables containing mean and standard deviation using yje select function:
-meanstddf <- select(mergedf, 1:3, contains("mean"), contains("std"))
-
-#check the features of the new dataframe:
-str(meanstddf)
-dim(meanstddf)
-head(meanstddf)[1:5,1:5]
+# Select the variables containing mean and standard deviation using the "select" function:
+meandf <- select(mergedf, 1:2, matches("[Mm]ean"), contains("std"))
+dim(meandf)
+head(meandf)[,1:5]
 
 # 3. Uses descriptive activity names to name the activities in the data set
+# Use "left-join" to merge the meandf and activity_labels dataframes joining by "activity_label":
+meandf2 <- left_join(meandf,activity_labels, "activity_label")
+dim(meandf2)
+head(meandf2)[,85:89]
 
-# Use "activity_labels" data read in at the beginning.
-# Use "left-join" to merge the meanstddf and activity_labels dataframes 
-# by "activity_label", and rearrange the columns:
-meanstddf2 <- left_join(meanstddf,activity_labels, "activity_label")
-
-#check the features of the new meanstddf2 dataframe:
-head(meanstddf2)[1:5,1:5]
-dim(meanstddf2)
-
-# Replace "activity_label" with the "activity" column 
-# by moving it to the second column, assign dataframe to meanstddf3:
-meanstddf3 <- meanstddf2[c(1, 89, 3:88)]
-head(meanstddf3)[1:5,1:5]
-dim(meanstddf3)
+# Rearrange the columns: moving "activity_label" to column 2 and assign dataframe to meandf3:
+meandf3 <- meandf2[c(1, 89, 3:88)]
+head(meandf3)[,1:5]
+dim(meandf3)
 
 # 4.Appropriately labels the data set with descriptive variable names.
-
 # Load the stringr package
+
 library(stringr)
 
-# Replace the shortened words with actual words, t with Time, f and -freq() with Frequency,
-# -mean() with Mean, -std() with STD:
+# Replace abbreviations in variable names with actual words: BodyBody, with Body; 
+# Acc with Accelerometer; Gyro with Gyroscope; Mag with Magnitude; t with Time; 
+# f and -freq() with Frequency; tBody with TimeBody; angle with Angle;
+# gravity with Gravity, -mean() with Mean, and -std() with STD:
 
-names(meanstddf3) <- gsub("Acc", "Accelerometer",names(meanstddf3)) %>% 
+names(meandf3) <- gsub("Acc", "Accelerometer",names(meandf3)) %>% 
         str_replace_all("Gyro", "Gyroscope") %>% 
         str_replace_all("BodyBody", "Body") %>%
         str_replace_all("Mag", "Magnitude") %>% 
@@ -184,23 +133,21 @@ names(meanstddf3) <- gsub("Acc", "Accelerometer",names(meanstddf3)) %>%
         str_replace_all("angle", "Angle") %>% 
         str_replace_all("gravity", "Gravity") %>%
         print
-#check the features of the new meanstddf3 dataframe:
-str(meanstddf3)
-dim(meanstddf3)
-head(meanstddf3)[1:5,1:5]
+
+#check the features of the new meandf3 dataframe:
+dim(meandf3)
+head(meandf3)[,1:10]
+
 # 5.From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
 # Use dplyr to group by subject and activity
-mean_activity<- meanstddf3 %>% 
+mean_activity<- meandf3 %>% 
         group_by(subject,activity) %>% 
         # summarise accross the variables that are numeric by mean, 
         # prepend the label "mean_" before each column
         summarise(across(where(is.numeric), ~ mean(.x), .names = "mean_{.col}")) %>% 
         print
-
-str(mean_activity)
-dim(mean_activity)
 
 # create the tidy data set as a .txt file
 write.table(mean_activity, "mean_activity.txt",row.name=FALSE)
